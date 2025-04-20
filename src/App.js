3aefda6c87b691be4f2a2d205afa462e23d9d801
LeandroMauricci <139007271+LeandroMauricci@users.noreply.github.com>
2025-04-19 22:10:00 -0500

    ...

--- src/App.js
+++ src/App.js
@@ -55,11 +55,11 @@ function slides() {
         <img id='slide1-1' src={require("./img/huanchaco1.jpg")} alt="" />
         <img id='slide1-2' src={require("./img/limpieza.webp")} alt="" />
         <img id='slide1-3' src={require("./img/huanchaco3.webp")} alt="" />
-        <div>
-        <h1 id='slideText1'>Tú puedes ser parte del <span className='celeste'>cambio</span></h1>
 
+      </div>
+      <div id='hijo'>
+        <h1 id='slideText1'>Tú puedes ser parte del <span className='celeste'>cambio</span></h1>
         <h5 id='slideText1' className='mt-2'><span className='grey'>Participa en una jornada de limpieza o en taller de sensibilización</span></h5>
-        </div>
       </div>
     </section>
   );
